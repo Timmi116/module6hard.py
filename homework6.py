90baf6d3a3da5dd4fef406c0be02faf63c997cc4
Timofei <timmi202116202@gmail.com>
2024-08-09 20:22:36 +0300

    Update homework6.py

--- homework6.py
+++ homework6.py
@@ -1,17 +1,102 @@
-my_dict = {'Den': 1990, 'Max': 1992, 'Sveta': 1994 }
-print(my_dict)
-print(my_dict['Max'])
-print(my_dict.get('Kolya', 'Такого значения нет'))
-my_dict.update({'Masha': 1996, 'Katya': 1998})
-print(my_dict)
-a = my_dict.pop('Den')
-print(a)
-print(my_dict)
+from turtle import color
+from math import pi, sqrt
 
-my_set = {1, 1, 1, 5, 5, 5, 'Urban', 'Hom', 'Hom', 'Urban'}
-print(my_set)
-print(my_set.add('Mama'))
-print(my_set.add(8))
-print(my_set)
-print(my_set.discard(1))
-print(my_set)
+class Figure:
+    sides_count = 0
+
+    def __init__(self, color, *sides):
+        self.__color = color
+        self.__sides = sides
+
+    def get_color(self):
+        return self.__color
+
+    def __is_valid_color(self, r, g, b):
+        if 0 < r <= 255 and 0 < g <= 255 and 0 < b <= 255:
+            return r, g, b
+        else:
+            return self.__color
+
+    def set_color(self, r, g, b):
+        new_color = self.__is_valid_color(r, g, b)
+        self.__color = list(new_color)
+        return self.__color
+
+    def __is_valid_sides(self, *new_sides):
+        for i in new_sides:
+            if i > 0:
+                if len(new_sides) == self.sides_count:
+                    return True
+
+                else:
+                    return False
+
+    def get_sides(self):
+        return self.__sides
+
+    def __len__(self):
+        return sum(self.__sides)
+
+    def set_sides(self, *new_sides):
+        for j in new_sides:
+            if j != self.__is_valid_sides(j):
+                self.__sides = list(new_sides)
+                return self.__sides
+
+class Circle(Figure):
+    sides_count = 1
+
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.__sides = sides
+        self.__radius = self.__sides[0] / (2 * pi)
+
+    def get_square(self):
+        s = (self.__radius ** 2) * pi
+        return s
+
+
+class Cube(Figure):
+    sides_count = 12
+
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.__sides = [sides[0]] * self.sides_count
+        print(self.__sides)
+
+    def get_volume(self):
+        V = self.__sides[0] ** 3
+        return V
+
+class Triangle(Figure):
+    sides_count = 3
+
+    def __init__(self, color, *sides):
+        super().__init__(color, *sides)
+        self.sides = sides
+        p = (self.sides[0] + self.sides[1] + self.sides[2]) / 2
+        self.__height = 2 * (sqrt(p * (p - self.sides[0]) * (p - self.sides[1]) * (p - self.sides[2]))) / self.sides[0]
+
+    def get_square(self):
+        St = (self.__height * self.sides[0]) / 2
+        return St
+
+circle1 = Circle((200, 200, 100), 10)
+cube1 = Cube((222, 35, 130), 6)
+
+
+circle1.set_color(55, 66, 77)
+print(circle1.get_color())
+cube1.set_color(300, 70, 15)
+print(cube1.get_color())
+
+cube1.set_sides(5, 3, 12, 4, 5)
+print(cube1.get_sides())
+circle1.set_sides(15)
+print(circle1.get_sides())
+
+print(len(circle1))
+
+print(cube1.get_volume())
+
+
